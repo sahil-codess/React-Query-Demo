3d0ba71f3bd35755e1e14787cb84a0dde6dd9fcc
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-21 10:50:15 +0430

    handling data fetching error using traditonal ways(using usestate and useeffect hooks)

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -5,16 +5,24 @@ import { useState, useEffect } from "react"
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [data, setData] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then((res) => {
+    axios.get('http://localhost:4000/superheroes1').then((res) => {
       setData(res.data)
+      setIsLoading(false)
+    }).catch((error) => {
+      setError(error.message)
       setIsLoading(false)
     })
   }, [])
 
   if(isLoading) {
     return <h2>Loading....</h2>
+  }
+
+  if (error) {
+    return <h2>{error}</h2>
   }
   return (
     <>
