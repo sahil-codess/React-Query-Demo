0bce09ad71a21ce8b77bafc842bcbafd610ab4e2
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-21 10:42:55 +0430

    created the traditional way of fetching data without using reactQuery

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,5 +1,27 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+
 export const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheroes').then((res) => {
+      setData(res.data)
+      setIsLoading(false)
+    })
+  }, [])
+
+  if(isLoading) {
+    return <h2>Loading....</h2>
+  }
   return (
-    <div>SuperHeroesPage</div>
+    <>
+        <h2> Super Heroes Page</h2>
+        {data.map((hero) => {
+          return <div key={hero.name}>{hero.name}</div>
+        })}
+    </>
   )
 }
