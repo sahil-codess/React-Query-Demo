6e37eb6399e1ed805ed5c1036e48af56442adc74
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-22 10:35:00 +0430

    data transformation using select function inside reactQuery to transform an object of data to an array of of just names

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useQuery } from "react-query"
 
 const fetchData = () =>{
-  return axios.get('http://localhost:4000/superheroes1')
+  return axios.get('http://localhost:4000/superheroes')
 }
 
 export const RQSuperHeroesPage = () => {
@@ -16,7 +16,10 @@ export const RQSuperHeroesPage = () => {
 
 
   const {isLoading, data, isError, error, isFetching, refetch} = useQuery('super-heroes', fetchData,
-   { onSuccess, onError})
+   { onSuccess, onError, select: (data) => {
+    const superHeroNames = data.data.map((hero) => hero.name)
+    return superHeroNames
+   }})
 
   console.log({isLoading, isFetching})
 
@@ -30,8 +33,11 @@ export const RQSuperHeroesPage = () => {
     <>
         <h2>RQ Super Heroes Page</h2>
         <button onClick={refetch}>Fetch Superheroes</button>
-        {data?.data.map((hero) => {
+        {/* {data?.data.map((hero) => {
           return <div key={hero.name}>{hero.name}</div>
+        })} */}
+        { data.map((heroName) => {
+          return <div key={heroName}>{heroName}</div>
         })}
     </>
   )
