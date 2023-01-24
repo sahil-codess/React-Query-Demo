71561a9b1aafade0d333ba3c7967703904f50f4b
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-24 18:29:42 +0430

    dependant queries, it is when a query is dependant on return of another query

--- src/App.js
+++ src/App.js
@@ -8,40 +8,42 @@ import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
 import { RQSuperHeroPage } from './components/RQSuperHero.Page';
 import { ParallelQueriesPage } from './components/ParallelQueries.page';
 import { DynamicParallelPage } from './components/DynamicParallel.Page';
+import { DependantQueriesPage } from './components/DependantQueries.Page';
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <Link to='/super-heroes'>Traditional Super Heroes Page</Link>
-                </li>
-                <li>
-                  <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-                </li>
-              </ul>
-            </nav>
-            <Routes>
-              <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage />}></Route>
-              <Route path='/rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1, 4]} />}></Route>
-              <Route path='/rq-parallel' element={<ParallelQueriesPage />}></Route>
-              <Route path='/super-heroes' element={<SuperHeroesPage />}></Route>
-              <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />}></Route>
-              <Route path='/' element={<HomePage />}></Route>
-            </Routes>
-          </div>
-          <ReactQueryDevtools InitialIsOpen={false} position='bottom-right' />
-        </Router>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes Page</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage />}></Route>
+            <Route path='/rq-dependant' element={<DependantQueriesPage email='qaiserfan2@gmail.com' />}></Route>
+            <Route path='/rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1, 4]} />}></Route>
+            <Route path='/rq-parallel' element={<ParallelQueriesPage />}></Route>
+            <Route path='/super-heroes' element={<SuperHeroesPage />}></Route>
+            <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />}></Route>
+            <Route path='/' element={<HomePage />}></Route>
+          </Routes>
+        </div>
+        <ReactQueryDevtools InitialIsOpen={false} position='bottom-right' />
+      </Router>
     </QueryClientProvider>
-    
+
   );
 }
 
